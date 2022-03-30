ecce9cda2f502e328abf73f927f1dbb5200c233f
vbetsun <vlad.betcun@gmail.com>
2022-03-30 20:58:13 +0300

    feat: finish main functionality

--- calculator.go
+++ calculator.go
@@ -1,6 +1,9 @@
 package calculator
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 type Option int
 
@@ -10,11 +13,53 @@ const (
 )
 
 var (
-	ErrInvalidStrike  = errors.New("calculator: strike should be >= 0")
-	ErrInvalidPremium = errors.New("calculator: premium should be >= 0")
+	ErrInvalidStrike       = errors.New("calculator: strike should be >= 0")
+	ErrInvalidStoke        = errors.New("calculator: stoke should be >= 0")
+	ErrInvalidPremium      = errors.New("calculator: premium should be >= 0")
+	ErrInvalidInterestRate = errors.New("calculator: interest rate should be >= 0 and <= 1")
+	ErrInvalidVolatility   = errors.New("calculator: volatility should be >= 0 and <= 1")
+	ErrInvalidTimeToExpire = errors.New("calculator: time to expire should be >= 0")
+	ErrInvalidDividend     = errors.New("calculator: dividend should be >= 0 and <= 1")
 )
 
-func BreakEvenPoint(opt Option, str, pr float32) (float32, error) {
+type BlackScholesModel struct {
+	Option
+	Strike       float64
+	Stock        float64
+	InterestRate float64
+	Volatility   float64
+	TimeToExpire float64
+	Dividend     float64
+}
+
+func (bsm BlackScholesModel) Price() (float64, error) {
+	if bsm.Strike < 0 {
+		return 0, ErrInvalidStrike
+	}
+	if bsm.Stock < 0 {
+		return 0, ErrInvalidStoke
+	}
+	if bsm.InterestRate < 0 || bsm.InterestRate > 1 {
+		return 0, ErrInvalidInterestRate
+	}
+	if bsm.Volatility < 0 || bsm.Volatility > 1 {
+		return 0, ErrInvalidVolatility
+	}
+	if bsm.TimeToExpire < 0 {
+		return 0, ErrInvalidTimeToExpire
+	}
+	if bsm.Dividend < 0 || bsm.Dividend > 1 {
+		return 0, ErrInvalidDividend
+	}
+	d1 := math.Log(bsm.Stock/bsm.Strike) + bsm.InterestRate - bsm.Dividend + math.Pow(bsm.Volatility, 2)/2*bsm.TimeToExpire/bsm.Volatility*math.Sqrt(bsm.TimeToExpire)
+	d2 := d1 - bsm.Volatility*math.Sqrt(bsm.TimeToExpire)
+	if bsm.Option == Call {
+		return bsm.Stock*math.Pow(math.E, -bsm.Dividend*bsm.TimeToExpire)*normDist(d1) - bsm.Strike*math.Pow(math.E, -bsm.InterestRate*bsm.TimeToExpire)*normDist(d2), nil
+	}
+	return bsm.Strike*math.Pow(math.E, -bsm.InterestRate*bsm.TimeToExpire)*normDist(-d2) - bsm.Stock*math.Pow(math.E, -bsm.Dividend*bsm.TimeToExpire)*normDist(-d1), nil
+}
+
+func BreakEvenPoint(opt Option, str, pr float64) (float64, error) {
 	if str < 0 {
 		return 0, ErrInvalidStrike
 	}
@@ -26,4 +71,47 @@ func BreakEvenPoint(opt Option, str, pr float32) (float32, error) {
 		return str + pr, nil
 	}
 	return str - pr, nil
+}
+
+func PayoffFromBuying(opt Option, str, st, pr float64) (float64, error) {
+	if str < 0 {
+		return 0, ErrInvalidStrike
+	}
+	if st < 0 {
+		return 0, ErrInvalidStoke
+	}
+	if pr < 0 {
+		return 0, ErrInvalidPremium
+	}
+	if opt == Call {
+		return math.Max(st-str, 0) - pr, nil
+	}
+	return math.Max(str-st, 0) - pr, nil
+}
+
+func PayoffFromSelling(opt Option, str, st, pr float64) (float64, error) {
+	if str < 0 {
+		return 0, ErrInvalidStrike
+	}
+	if st < 0 {
+		return 0, ErrInvalidStoke
+	}
+	if pr < 0 {
+		return 0, ErrInvalidPremium
+	}
+	if opt == Call {
+		return pr - math.Max(st-str, 0), nil
+	}
+	return pr - math.Max(str-st, 0), nil
+}
+
+func normDist(z float64) float64 {
+	t := 1 / (1 + 0.2316419*math.Abs(z))
+	t2 := math.Pow(t, 2)
+	y := t * (0.319381530 - 0.356563782*t + (1.781477937-1.821255978*t+1.330274429*t2)*t2)
+
+	if z > 0 {
+		return 1 - math.Exp(-(math.Log(2*math.Pi)+math.Pow(z, 2))*0.5)*y
+	}
+	return math.Exp(-(math.Log(2*math.Pi)+math.Pow(-z, 2))*0.5) * y
 }
--- calculator_test.go
+++ calculator_test.go
@@ -1,23 +1,28 @@
 package calculator
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestBreakEvenPoint(t *testing.T) {
 	type args struct {
 		opt Option
-		str float32
-		pr  float32
+		str float64
+		pr  float64
 	}
 	tests := []struct {
 		name    string
 		args    args
-		want    float32
+		want    float64
 		wantErr bool
 	}{
-		{name: "Call option with premium", args: args{opt: Call, str: 50, pr: 10}, want: 60},
-		{name: "Call option without premium", args: args{opt: Call, str: 50}, want: 50},
-		{name: "Call option with incorrect premium", args: args{opt: Call, str: 50, pr: -10}, wantErr: true},
-		{name: "Call option with incorrect strike", args: args{opt: Call, str: -50, pr: 10}, wantErr: true},
+		{name: "option with incorrect 'strike'", args: args{str: -50}, wantErr: true},
+		{name: "'option with incorrect 'premium'", args: args{pr: -10}, wantErr: true},
+		{name: "'call' with 'premium'", args: args{opt: Call, str: 50, pr: 10}, want: 60},
+		{name: "'call' without 'premium'", args: args{opt: Call, str: 50}, want: 50},
+		{name: "'put' with 'premium'", args: args{opt: Put, str: 50, pr: 10}, want: 40},
+		{name: "'put' without 'premium'", args: args{opt: Put, str: 50}, want: 50},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -28,6 +33,136 @@ func TestBreakEvenPoint(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("BreakEvenPoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPayoffFromBuying(t *testing.T) {
+	type args struct {
+		opt Option
+		str float64
+		st  float64
+		pr  float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		{name: "incorrect strike", args: args{str: -50}, wantErr: true},
+		{name: "incorrect stoke", args: args{st: -50}, wantErr: true},
+		{name: "incorrect premium", args: args{pr: -10}, wantErr: true},
+		{name: "profit from Call when 'strike' without 'premium' is less than 'stock'", args: args{opt: Call, str: 50, st: 70}, want: 20},
+		{name: "profit from Call when 'strike' with 'premium' is less than 'stock'", args: args{opt: Call, str: 50, st: 70, pr: 10}, want: 10},
+		{name: "no profit from Call when 'strike' without 'premium' is more than 'stock'", args: args{opt: Call, str: 50, st: 40}, want: 0},
+		{name: "lose from Call when 'strike' with 'premium' is more than 'stock'", args: args{opt: Call, str: 50, st: 10, pr: 10}, want: -10},
+		{name: "profit from Put when 'strike' without 'premium' is more than 'stock'", args: args{opt: Put, str: 50, st: 40}, want: 10},
+		{name: "profit from Put when 'strike' with 'premium' is more than 'stock'", args: args{opt: Put, str: 50, st: 30, pr: 10}, want: 10},
+		{name: "no profit from Put when 'strike' without 'premium' is less than 'stock'", args: args{opt: Put, str: 50, st: 60}, want: 0},
+		{name: "lose from Put when 'strike' with 'premium' is less than 'stock'", args: args{opt: Put, str: 50, st: 70, pr: 10}, want: -10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := PayoffFromBuying(tt.args.opt, tt.args.str, tt.args.st, tt.args.pr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PayoffFromBuying() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("PayoffFromBuying() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPayoffFromSelling(t *testing.T) {
+	type args struct {
+		opt Option
+		str float64
+		st  float64
+		pr  float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		{name: "incorrect strike", args: args{str: -50}, wantErr: true},
+		{name: "incorrect stoke", args: args{st: -50}, wantErr: true},
+		{name: "incorrect premium", args: args{pr: -10}, wantErr: true},
+		{name: "profit from Call when 'strike' with 'premium' is more than 'stock'", args: args{opt: Call, str: 50, st: 40, pr: 10}, want: 10},
+		{name: "no profit from Call when 'strike' without 'premium' is more than 'stock'", args: args{opt: Call, str: 50, st: 40}, want: 0},
+		{name: "lose from Call when 'strike' without 'premium' is more than 'stock'", args: args{opt: Call, str: 50, st: 60}, want: -10},
+		{name: "lose from Call when 'strike' with 'premium' is less than 'stock'", args: args{opt: Call, str: 50, st: 70, pr: 10}, want: -10},
+		{name: "infinity lose from Call when 'stock' gets infinity raise", args: args{opt: Call, str: 50, st: math.Inf(1)}, want: math.Inf(-1)},
+
+		{name: "profit from Put when 'strike' with 'premium' is less than 'stock'", args: args{opt: Put, str: 50, st: 70, pr: 10}, want: 10},
+		{name: "no profit from Put when 'strike' without 'premium' is more than 'stock'", args: args{opt: Put, str: 50, st: 60}, want: 0},
+		{name: "lose from Put when 'strike' without 'premium' is more than 'stock'", args: args{opt: Put, str: 50, st: 40}, want: -10},
+		{name: "lose from Put when 'strike' with 'premium' is more than 'stock'", args: args{opt: Put, str: 50, st: 30, pr: 10}, want: -10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := PayoffFromSelling(tt.args.opt, tt.args.str, tt.args.st, tt.args.pr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PayoffFromSelling() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("PayoffFromSelling() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBlackScholesModel_Price(t *testing.T) {
+	type fields struct {
+		Option       Option
+		Strike       float64
+		Stock        float64
+		InterestRate float64
+		Volatility   float64
+		TimeToExpire float64
+		Dividend     float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    float64
+		wantErr bool
+	}{
+		{name: "invalid strike", fields: fields{Strike: -1}, wantErr: true},
+		{name: "invalid stoke", fields: fields{Stock: -1}, wantErr: true},
+		{name: "invalid interest rate", fields: fields{InterestRate: 2}, wantErr: true},
+		{name: "invalid volatility", fields: fields{Volatility: 2}, wantErr: true},
+		{name: "invalid time to expire", fields: fields{TimeToExpire: -1}, wantErr: true},
+		{name: "invalid dividend", fields: fields{Dividend: 2}, wantErr: true},
+		{name: "call with dividends", fields: fields{Option: Call, Strike: 58, Stock: 60, InterestRate: 0.035, Volatility: 0.2, TimeToExpire: 0.5, Dividend: 0.0125}, want: 4.624545765201692},
+		{name: "call without dividends", fields: fields{Option: Call, Strike: 58, Stock: 60, InterestRate: 0.035, Volatility: 0.2, TimeToExpire: 0.5}, want: 4.838950566319454},
+		{name: "put with dividends", fields: fields{Option: Put, Strike: 58, Stock: 60, InterestRate: 0.035, Volatility: 0.2, TimeToExpire: 0.5, Dividend: 0.0125}, want: 1.9922059963722596},
+		{name: "put without dividends", fields: fields{Option: Put, Strike: 58, Stock: 60, InterestRate: 0.035, Volatility: 0.2, TimeToExpire: 0.5}, want: 1.8327802348937041},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bsm := BlackScholesModel{
+				Option:       tt.fields.Option,
+				Strike:       tt.fields.Strike,
+				Stock:        tt.fields.Stock,
+				InterestRate: tt.fields.InterestRate,
+				Volatility:   tt.fields.Volatility,
+				TimeToExpire: tt.fields.TimeToExpire,
+				Dividend:     tt.fields.Dividend,
+			}
+			got, err := bsm.Price()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BlackScholesModel.Price() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("BlackScholesModel.Price() = %v, want %v", got, tt.want)
 			}
 		})
 	}
